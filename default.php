626d7262156c855101ca0106bdac51a254631c59
Gabrie1Alves <gabrielalvesresenderocha@gmail.com>
2021-10-08 09:36:40 -0300

    Pagina default, inicial

---
+++ default.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>PREVIA</title>
+        <!--<link rel="sortcut icon" href="./img/icone.png" type="image/x-icon" />; COLOCAR LOGO AQUI -->
+        <link rel="stylesheet" href="./css/index.css">
+        <link rel="stylesheet" href="./css/headerFooter.css">
+        <link rel="stylesheet" href="./css/whatsapp.css">
+    </head>
+
+    <body style="margin:0px">
+    
+    <a class="whatsapp-link" href="https://api.whatsapp.com/send?phone=5541995465020&text=Ol%C3%A1,%20quero%20marcar%20uma%20vistoria!" target="_blank"> <i class="fa fa-whatsapp"></i> </a>
+
+
+        <div class="cab">
+            <p class="pcab"><a class="cabHover" href="#"><b>ePrivilège CAR</b></a> <a class="cabHover" href="">Seminovos</a> <a class="cabHover" href="">Venda seu carro</a> <a class="cabHover" href="">Quem somos</a></p>
+        </div>
+
+        <div class="inicio">
+            <div class="iniTxt">
+                <div class="iniTxt1">
+                    <h1 class="iniH1">
+                        Não perca tempo, <br>
+                        livre-se de riscos, <br>
+                        tudo sem burocracia! <br>
+
+                        <a class="botINI2" href="">Venda seu carro</a> <br>
+                        <a class="botINI" href="">Compre seu carro</a>
+                    </h1>
+                </div>
+            </div>
+            <div class="iniImg">
+                <img  class="imagemINI" src="./imagem/carros/volvofrente.png" alt="">
+            </div>
+        </div>
+
+        <div class="compre">
+            <div class="video">
+                <div style="float: right;">
+                    <h1 class="videoH1">
+                        Seminovos, <br>
+                        <span style="color: #ff4700;">Sem surpresas indesejadas.</span>
+                    </h1>
+                    <iframe src="https://www.youtube.com/embed/3P4-9mz7-ic" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="textoCompre">
+                <div class="textoCompreDentro">
+                    <h1 class="textoCompreH1">
+                        Compre seu carro
+                    </h1> 
+                    <ul>
+                        <li class="compreLI">Certificação em 100% dos carros.</li> <br>
+                        <li class="compreLI">Descrição de todos os detalhes do carro.</li> <br>
+                        <li class="compreLI">Documentação em dia.</li> <br>
+                        <li class="compreLI">Acompanhamento durante todo o processo.</li> <br>
+                        <li class="compreLI">Entregamos na sua casa.</li> <br>
+                    </ul>  
+                    <a href=""><button class="botaoCompra">Compre seu carro</button></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="venda">
+            <div class="vendaEsquerda">
+                <div style="width: 600px; float:right; margin-left:20px">
+                    <h1 style="margin-left: 15px;">
+                        <span class="vendaCor" style="font-size: 40px;"> Venda seu carro, </span><br>
+                        <span>vamos até você para comprar seu carro!</span> 
+                    </h1>
+                    <ul>
+                        <li class="vendaLI">Obtenha a melhor oferta pelo seu carro.</li>
+                        <li class="vendaLI">Pagamento seguro e imediato mediante a transferência.</li>
+                        <li class="vendaLI">Retirada na porta da sua casa.</li>
+                        <li class="vendaLI">Avaliamos seu carro em sua casa.</li>
+                        <li class="vendaLI">Entregamos na sua casa.</li>
+                    </ul> 
+                    <a href=""><button class="botaoVenda">Venda seu carro</button></a>
+                    
+                </div>
+            </div>
+            <div class="vendaDireita">
+                <img class="imgVenda"  src="./imagem/carros/mustang.png" alt="">
+            </div>
+        </div>
+        
+
+
+
+        <div class="coment">
+            <h1 style="text-align: center;"> Colocar reclame aqui</h1>
+        </div>
+
+
+
+
+        <div class="perguntas">
+            <h1 style="text-align: center;" class="vendaCor">Perguntas frequentes</h1>
+
+            <div class="pEsquerda">
+                <div class="pergunta">
+                    <h1 style="text-align: center;">
+                        ePRIVILÈGE CAR é apenas um site de classificados?
+                    </h1>
+                    <h2 style="text-align: justify;" class="h2perguntas">
+                        Não, nós somos uma empresa especialista na prestação de serviço 
+                        para as pessoas que desejam vender seus carros. Temos 2 opções de 
+                        serviços: repasse – dinheiro na hora, e vendemos seu veículos da forma convencional.
+                    </h2>
+                </div>
+                <div class="pergunta">
+                    <h1 style="text-align: center;">
+                        Como funciona e quanto tempo dura todo o processo?
+                    </h1>
+                    <h2 style="text-align: justify;" class="h2perguntas">
+                        No processo de repasse você recebe uma proposta em 24h e no convencional 
+                        trabalhamos anunciando seu carro sem um prazo para a primeira oferta.
+                    </h2>
+                </div>
+                <div class="pergunta">
+                    <h1 style="text-align: center;">
+                        Quais são os custos do processo?
+                    </h1>
+                    <h2 style="text-align: justify;" class="h2perguntas">
+                        Para o repasse cobramos uma corretagem de 4,1% sobre o valor da venda mais o 
+                        valor da vistoria cautelar e para a venda convencional 6,0% mais o valor da vistoria cautelar.
+                    </h2>
+                </div>
+                <div class="pergunta">
+                    <h1 style="text-align: center;">
+                        Quem compra os carros?
+                    </h1>
+                    <h2 style="text-align: justify;" class="h2perguntas">
+                        No repasse são nossos parceiros, profissionais do setor. Na venda convencional será o cliente final.
+                    </h2>
+                </div>
+                <div class="pergunta">
+                    <h1 style="text-align: center;">
+                        Quanto tempo leva a avaliação e quais documentos preciso levar no dia?
+                    </h1>
+                    <h2 style="text-align: justify;" class="h2perguntas">
+                        A avaliação será agendado dentro das próximas 24h. Os 
+                        documentos para a avaliação são CNH, comprovante de endereço, o DUT e o CRV.
+                    </h2>
+                </div>
+                <div class="pergunta">
+                    <h1 style="text-align: center;">
+                        O que é uma avaliação e por que fazer uma?
+                    </h1>
+                    <h2 style="text-align: justify;" class="h2perguntas">
+                        A avaliação é importante para a correta valorização de 
+                        carro e pela segurança dos vendedores e compradores.
+                    </h2>
+                </div>
+                <div class="pergunta">
+                    <h1 style="text-align: center;">
+                        Por que não consigo vender meu carro pelo preço Tabela FIPE na modalidade de repasse?
+                    </h1>
+                    <h2 style="text-align: justify;" class="h2perguntas">
+                        Na modalidade de repasse, existe a necessidade dos lojista 
+                        (profissionais do setor) ter margem para lucrar na revenda do carro.
+                    </h2>   
+                </div>
+
+
+            </div>
+            
+            <div class="pDireita">
+                <div class="pergunta">
+                    <h1 style="text-align: center;">
+                        Quais são as condições do veículo para vender na ePRIVILÈGE CAR?
+                    </h1>
+                    <h2 style="text-align: justify;" class="h2perguntas">
+                        O veículo de estar limpo.
+                    </h2>
+                    
+                </div>
+                <div class="pergunta">
+                    <h1 style="text-align: center;">
+                        Como é feito o pagamento após aceitar a oferta?
+                    </h1>
+                    <h2 style="text-align: justify;" class="h2perguntas">
+                        Realizamos a transferência bancária (TED) e você confirma e entrega as chaves.
+                    </h2>
+                </div>
+                <div class="pergunta">
+                    <h1 style="text-align: center;">
+                        Como funciona transferência de documentação?
+                    </h1>
+                    <h2 style="text-align: justify;" class="h2perguntas">
+                        A ePRIVILÈGE cuida de toda burocracia e cuide de sua segurança.
+                    </h2>
+                </div>
+                <div class="pergunta">
+                    <h1 style="text-align: center;">
+                        Por que preciso passar minhas informações?
+                    </h1>
+                    <h2 style="text-align: justify;" class="h2perguntas">
+                        Desta forma acelera o processo para a venda de seu carro e 
+                        aumenta a segurança das pessoas envolvidas no processo de venda de seu carro.
+                    </h2>
+                </div>
+                <div class="pergunta">
+                    <h1 style="text-align: center;">
+                        O que acontece se não gostar da proposta?
+                    </h1>
+                    <h2 style="text-align: justify;" class="h2perguntas">
+                        Nada, apenas ficamos tristes em não poder ajudar você.
+                    </h2>
+                </div>
+                <div class="pergunta">
+                    <h1 style="text-align: center;">
+                        Vocês compram carro com dívida, financiamento, DETRAN ou Leasing?
+                    </h1>
+                    <h2 style="text-align: justify;" class="h2perguntas">
+                        Sim, todas as dividas serão abatidas do valor de venda.
+                    </h2>
+                </div>
+                <div class="pergunta">
+                    <h1 style="text-align: center;">
+                        O pagamento é feito a vista?
+                    </h1>
+                    <h2 style="text-align: justify;" class="h2perguntas">
+                        Sim, feito através de transferência bancária após o reconhecimento de firma no cartório.
+                    </h2>
+                </div>
+            </div> 
+        </div>
+
+        <div class="rodape">
+            <h1 class="h1rodape" style="padding: 5px 0px 0px 5px;"><a class="arodape1" href="">Atendimento 24 horas por dia</a></h1>
+            <div class="imgCentro">
+                <a href=""><img class="imgrodape" src="./imagem/imgLogos/facebook.png" alt="Facebook"></a>
+                <a href=""><img class="imgrodape" src="./imagem/imgLogos/linkedin.png" alt="Linkedin"></a>
+                <a href=""><img class="imgrodape" src="./imagem/imgLogos/instagram.png" alt="Instagram"></a>
+                <a href=""><img class="imgrodape" src="./imagem/imgLogos/whatsapp.png" alt="Whatsapp"></a>
+            </div>
+            <h1 class="h1rodape2" style="text-align: center;"><a class="aRodape" href="">Compre seu carro</a> <a class="aRodape" href="">Venda seu carro</a>  <a class="aRodape" href="">Contato</a> <a class="aRodape" href="">Política do site</a></h1>
+            <h1 style="text-align: center; font-size:15px; margin:0px;">@ePrivilegeCAR</h1>
+        </div>
+    </body>
+</html>
+
+<!--DESCER PRA BAIXO QUANDO A TELA ENCURTAR MUITO-->
