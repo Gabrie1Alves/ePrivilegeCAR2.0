761b8a1fa458d50a818172463bf88a8a28b70c03
Gabrie1Alves <gabrielalvesresenderocha@gmail.com>
2021-10-25 15:05:05 -0300

    Exclusão das paginas: css, imagens, javascript. Dia: 25-10-2021

--- javascript/teste.js
+++
@@ -1,7 +0,0 @@
-//Procura dentro do documento form e add evento submit
-document.querySelector("form").addEventListener("submit", Event => {
-    console.log("Enviar o form")
-
-    //Não vai enviar o form
-    Event.preventDefault
-})
